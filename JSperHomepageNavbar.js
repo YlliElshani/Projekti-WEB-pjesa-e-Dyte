41a5d2d61b046570be80502ab1d24d6d032d7b16
Ylli Elshani <yllielshani6@gmail.com>
2021-02-03 23:02:51 +0100

    Shtohet stilizimi ne HomePage/ProductPage shtohet Javascript per funksionim te navbar, ndrryshim i vogel ne Login

---
+++ JSperHomepageNavbar.js
@@ -0,0 +1,27 @@
+console.dir(document);
+
+function openNav() {
+  document.getElementById("NavBarID").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("NavBarID").style.width = "0";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
