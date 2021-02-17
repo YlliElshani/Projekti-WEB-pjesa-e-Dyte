19553fdcc1dd6902919fdc00ee87a08db5841559
Ylli Elshani <yllielshani6@gmail.com>
2021-02-17 20:31:03 +0100

    Shtohet responsiviteti per Iphone X/IPad 768x1024

--- JS files/JSperHomepageNavbar.js
+++ JS files/JSperHomepageNavbar.js
@@ -1,7 +1,7 @@
 console.dir(document);
 
 function openNav() {
-  document.getElementById("NavBarID").style.width = "250px";
+  document.getElementById("NavBarID").style.width = "75%";
 }
 
 /* Set the width of the side navigation to 0 */
